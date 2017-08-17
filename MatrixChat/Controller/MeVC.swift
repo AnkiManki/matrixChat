1cf85fdc573e6960927744f109969da0b01d6289
Punpun <stefan.neotokyo@gmail.com>
2017-08-17 11:58:17 +0200

    Added MeVC

---
+++ MatrixChat/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  MatrixChat
+//
+//  Created by Stefan Markovic on 8/17/17.
+//  Copyright © 2017 Stefan Markovic. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signOutBtn(_ sender: UIButton) {
+        
+    }
+    
+
+}
