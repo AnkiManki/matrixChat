b45ceabe0350023e0d2e6d1c059c05907d76134f
Punpun <stefan.neotokyo@gmail.com>
2017-08-17 15:56:56 +0200

    Added CreateGroups view controller (code and UI)

---
+++ MatrixChat/Controller/CreateGroupsVC.swift
@@ -0,0 +1,37 @@
+//
+//  CreateGroupsVC.swift
+//  MatrixChat
+//
+//  Created by Stefan Markovic on 8/17/17.
+//  Copyright © 2017 Stefan Markovic. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchField: InsetTextField!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func doneBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+
+}
